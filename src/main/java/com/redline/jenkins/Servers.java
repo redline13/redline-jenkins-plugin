e1244d5c5ff7f1ab0f2a60b218fc9ff13af47770
Richard Friedman <rfriedman@themeetgroup.com>
2018-09-24 14:43:15 -0400

    Added all new server types

--- src/main/java/com/redline/jenkins/Servers.java
+++ src/main/java/com/redline/jenkins/Servers.java
@@ -61,7 +61,7 @@ public class Servers extends AbstractDescribableImpl<Servers> {
     public String getLocation() {
         return location;
     }
-    
+
     public String getSize() {
         return size;
     }
@@ -72,7 +72,7 @@ public class Servers extends AbstractDescribableImpl<Servers> {
         }
         return numberServers;
     }
-    
+
     public Integer getUsersPerServer(){
         if ( usersPerServer == null ){
             return 1;
@@ -103,7 +103,7 @@ public class Servers extends AbstractDescribableImpl<Servers> {
     public String getSecurityGroupIds() {
         return securityGroupIds;
     }
-    
+
 
     @Extension
     public static class DescriptorImpl extends Descriptor<Servers> {
@@ -112,7 +112,7 @@ public class Servers extends AbstractDescribableImpl<Servers> {
         public String getDisplayName() {
             return "";
         }
-        
+
         public ListBoxModel doFillLocationItems() {
             ListBoxModel items = new ListBoxModel();
             items.add("Virginia (us-east-1)","us-east-1");
@@ -129,43 +129,80 @@ public class Servers extends AbstractDescribableImpl<Servers> {
             items.add("Asia Pacific South (Mumbai) (ap-south-1)","ap-south-1");
             return items;
         }
-        
+
         public ListBoxModel doFillSizeItems(){
             ListBoxModel items = new ListBoxModel();
-            items.add("t2.micro","t2.micro");
-            items.add("t2.small","t2.small");
-            items.add("t2.medium","t2.medium");
-            items.add("t2.large","t2.large");
-            items.add("m3.medium","m3.medium");
-            items.add("m3.large","m3.large");
-            items.add("m3.xlarge","m3.xlarge");
-            items.add("m3.2xlarge","m3.2xlarge");
-            items.add("m4.large","m4.large");
-            items.add("m4.xlarge","m4.xlarge");
-            items.add("m4.2xlarge","m4.2xlarge");
-            items.add("m4.4xlarge","m4.4xlarge");
-            items.add("m4.10xlarge","m4.10xlarge");
-            items.add("c3.large","c3.large");
-            items.add("c3.xlarge","c3.xlarge");
-            items.add("c3.2xlarge","c3.2xlarge");
-            items.add("c3.4xlarge","c3.4xlarge");
-            items.add("c3.8xlarge","c3.8xlarge");
-            items.add("c4.large","c4.large");
-            items.add("c4.xlarge","c4.xlarge");
-            items.add("c4.2xlarge","c4.2xlarge");
-            items.add("c4.4xlarge","c4.4xlarge");
-            items.add("c4.8xlarge","c4.8xlarge");
-            items.add("r3.large","r3.large");
-            items.add("r3.xlarge","r3.xlarge");
-            items.add("r3.2xlarge","r3.2xlarge");
-            items.add("r3.4xlarge","r3.4xlarge");
-            items.add("r3.8xlarge","r3.8xlarge");
-            items.add("i2.xlarge","i2.xlarge");
-            items.add("i2.2xlarge","i2.2xlarge");
-            items.add("i2.4xlarge","i2.4xlarge");
-            items.add("i2.8xlarge","i2.8xlarge");            
+            items.add("t2.nano","t2.nano")
+            items.add("t2.micro","t2.micro")
+            items.add("t2.small","t2.small")
+            items.add("t2.medium","t2.medium")
+            items.add("t2.large","t2.large")
+            items.add("t2.xlarge","t2.xlarge")
+            items.add("t2.2xlarge","t2.2xlarge")
+            items.add("t3.nano","t3.nano")
+            items.add("t3.micro","t3.micro")
+            items.add("t3.small","t3.small")
+            items.add("t3.medium","t3.medium")
+            items.add("t3.large","t3.large")
+            items.add("t3.xlarge","t3.xlarge")
+            items.add("t3.2xlarge","t3.2xlarge")
+            items.add("m3.medium","m3.medium")
+            items.add("m3.large","m3.large")
+            items.add("m3.xlarge","m3.xlarge")
+            items.add("m3.2xlarge","m3.2xlarge")
+            items.add("m4.large","m4.large")
+            items.add("m4.xlarge","m4.xlarge")
+            items.add("m4.2xlarge","m4.2xlarge")
+            items.add("m4.4xlarge","m4.4xlarge")
+            items.add("m4.10xlarge","m4.10xlarge")
+            items.add("m4.16xlarge","m4.16xlarge")
+            items.add("m5.large","m5.large")
+            items.add("m5.xlarge","m5.xlarge")
+            items.add("m5.2xlarge","m5.2xlarge")
+            items.add("m5.4xlarge","m5.4xlarge")
+            items.add("m5.12xlarge","m5.12xlarge")
+            items.add("m5.24xlarge","m5.24xlarge")
+            items.add("c3.large","c3.large")
+            items.add("c3.xlarge","c3.xlarge")
+            items.add("c3.2xlarge","c3.2xlarge")
+            items.add("c3.4xlarge","c3.4xlarge")
+            items.add("c3.8xlarge","c3.8xlarge")
+            items.add("c4.large","c4.large")
+            items.add("c4.xlarge","c4.xlarge")
+            items.add("c4.2xlarge","c4.2xlarge")
+            items.add("c4.4xlarge","c4.4xlarge")
+            items.add("c4.8xlarge","c4.8xlarge")
+            items.add("c5.large","c5.large")
+            items.add("c5.xlarge","c5.xlarge")
+            items.add("c5.2xlarge","c5.2xlarge")
+            items.add("c5.4xlarge","c5.4xlarge")
+            items.add("c5.9xlarge","c5.9xlarge")
+            items.add("c5.18xlarge","c5.18xlarge")
+            items.add("r3.large","r3.large")
+            items.add("r3.xlarge","r3.xlarge")
+            items.add("r3.2xlarge","r3.2xlarge")
+            items.add("r3.4xlarge","r3.4xlarge")
+            items.add("r3.8xlarge","r3.8xlarge")
+            items.add("r4.large","r4.large")
+            items.add("r4.xlarge","r4.xlarge")
+            items.add("r4.2xlarge","r4.2xlarge")
+            items.add("r4.4xlarge","r4.4xlarge")
+            items.add("r4.8xlarge","r4.8xlarge")
+            items.add("r4.16xlarge","r4.16xlarge")
+            items.add("r5.large","r5.large")
+            items.add("r5.xlarge","r5.xlarge")
+            items.add("r5.2xlarge","r5.2xlarge")
+            items.add("r5.4xlarge","r5.4xlarge")
+            items.add("r5.12xlarge","r5.12xlarge")
+            items.add("r5.24xlarge","r5.24xlarge")
+            items.add("i2.xlarge","i2.xlarge")
+            items.add("i2.2xlarge","i2.2xlarge")
+            items.add("i2.4xlarge","i2.4xlarge")
+            items.add("i2.8xlarge","i2.8xlarge")
+            items.add("x1.16xlarge","x1.16xlarge")
+            items.add("x1.32xlarge","x1.32xlarge")
             return items;
         }
-        
+
     }
 }
